b7dd9ed8cc07b3d986eb6a6d499a9a81c133d8d0
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2022-12-01 19:10:18 +0530

    adding background tasks

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
 
 app = FastAPI()
-course_items = [{"course_name": "Python"}, {"course_name": "NodeJS"}, {"course_name": "Machine Learning"}]
 
-@app.get("/courses/")
-def read_courses(start: int = 0, end: int = 10):
-    return course_items[start : start + end]
+def write_notification(email: str, message=""):
+    with open("log.txt", mode="w") as email_file:
+        content = f"notification for {email}: {message}"
+        email_file.write(content)
+
+@app.post("/send-notification/{email}")
+def send_notification(email: str, background_tasks: BackgroundTasks):
+    background_tasks.add_task(write_notification, email, message="some notification")
+    return {"message": "notification sent in the background"}
+    
