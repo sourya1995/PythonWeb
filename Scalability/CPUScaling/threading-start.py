fd346c8b8b4b04e59fc7229b35182329e6e26caf
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-04-30 08:47:39 +0530

    Adding daemon thread

--- Scalability/CPUScaling/threading-start.py
+++ Scalability/CPUScaling/threading-start.py
@@ -4,6 +4,6 @@ def print_something(something):
     print(something)
 
 t = threading.Thread(target=print_something, args="hello")
+t.daemon = True #background thread
 t.start()
 print("thread started")
-t.join()
