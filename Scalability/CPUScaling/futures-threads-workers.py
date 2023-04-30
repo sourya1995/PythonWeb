ce1912b8909384e9cc9b8ab4e83b75342e04ce6d
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-04-30 09:16:38 +0530

    Adding processpoolexecutor

--- Scalability/CPUScaling/futures-threads-workers.py
+++ Scalability/CPUScaling/futures-threads-workers.py
@@ -5,7 +5,7 @@ def compute():
     return sum(
         [random.randint(1, 100) for i in range(1000000)])
 
-with futures.ThreadPoolExecutor(max_workers=8) as executor:
+with futures.ProcessPoolExecutor(max_workers=8) as executor:
     futs = [executor.submit(compute) for _ in range(8)]
 
 results = [f.result() for f in futs]
