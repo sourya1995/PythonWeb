e8364b149c94e5a422b5d589cd9ec45c3239c502
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-04-30 09:16:09 +0530

    Adding futures

---
+++ Scalability/CPUScaling/futures-threads-workers.py
@@ -0,0 +1,12 @@
+from concurrent import futures
+import random
+
+def compute():
+    return sum(
+        [random.randint(1, 100) for i in range(1000000)])
+
+with futures.ThreadPoolExecutor(max_workers=8) as executor:
+    futs = [executor.submit(compute) for _ in range(8)]
+
+results = [f.result() for f in futs]
+print("Results: %s" % results)
