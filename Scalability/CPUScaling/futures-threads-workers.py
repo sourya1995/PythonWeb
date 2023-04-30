e25bf94ba0255f04bbdea07de638f8459420faed
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-04-30 09:19:22 +0530

    Adding a ref comment

--- Scalability/CPUScaling/futures-threads-workers.py
+++ Scalability/CPUScaling/futures-threads-workers.py
@@ -5,7 +5,7 @@ def compute():
     return sum(
         [random.randint(1, 100) for i in range(1000000)])
 
-with futures.ProcessPoolExecutor() as executor:
+with futures.ProcessPoolExecutor() as executor: #max_workers=multiprocessing.cpu_count
     futs = [executor.submit(compute) for _ in range(8)]
 
 results = [f.result() for f in futs]
